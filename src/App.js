8ad4b4a044723f70edc8ba9e1e0059a896fe1a74
JesseRGit <jesseronkko@hotmail.com>
2019-01-14 15:00:19 +0200

    Fixed all Firebase problems, at working state now

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import firebase from '@firebase/app';
+import '@firebase/auth';
+import "@firebase/database";
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
